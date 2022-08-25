62d38197baed4c1e95f014bf958ac0c70f360d12
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-08-25 16:27:53 -0700

    Create resursive fibonacci function

---
+++ fibo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// Fibo function
+int fibo(int n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    else
+        return fibo(n - 1) + fibo(n - 2);
+}
